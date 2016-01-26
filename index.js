f8702b3c74cd40cb011be58605508bad46b643bc
beeva-joseGutierrez <jose.gutierrez@beeva.com>
2016-01-26 15:28:41 +0100

    Dojo resolved 2nd version

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 
 const long = 10;
-var Utils = require(__dirname+'/lib/Utils');
-var Arrays = Utils.prototype.Arrays(); // llamamos a función estática de Utils que nos devuelve la clase Arrays
-var ua = new Arrays(long); // llamamos a clase hija de Utils para arrays
-var randomData = ua.getRandom(); // array de 10 els random
+const Utils = require(__dirname+'/lib/Utils');
+// import * as Utils from (__dirname+'/Utils');
+const Arrays = Utils.Arrays(); // llamamos a función estática de Utils que nos devuelve la clase Arrays
+let ua = new Arrays(long); // llamamos a clase hija de Utils para arrays
+let randomData = ua.getRandom(); // array de 10 els random
 console.log(randomData);
 console.log(ua.findIndex(randomData,6)); // buscamos el índice del elemento 6 => random...
 console.log(ua.findIndex(randomData,777)); // buscamos el índice del elemento 777 => -1
 
-var object = ua.objectIt([7,4,6,9,8,3,2,1]); // pasa un array a un objeto de lo más tonto...
+let object = ua.objectIt([7,4,6,9,8,3,2,1]); // pasa un array a un objeto de lo más tonto...
 console.log(object.arr); // el array pasado
 console.log(object.list()); // el array en la clase
 console.log(object.getItem(5));
@@ -18,18 +19,18 @@ object = object.getItem(5);
 console.log("El objeto con índice "+object.index+" vale "+object.value);
 
 
-var Weather = Utils.Weather(); // llamamos a función estática de Utils que nos devuelve la clase Weather
-var uw = new Weather();
-var city = 'Barcelona';
+let Weather = Utils.Weather(); // llamamos a función estática de Utils que nos devuelve la clase Weather
+let uw = new Weather();
+let city = 'Barcelona';
 uw.get(city,function(err,w){
     if(!err) uw.write(city,w);
 });
 
 uw.print('Madrid');
 
-var u = new Utils();
-var word = "Prueba";
-var test = "eb";
+let u = new Utils();
+let word = "Prueba";
+let test = "eb";
 console.log(u.includes(word,test));
 console.log(u.includes(word,test,7));
 console.log(u.includes(word,word.substr(3)));
