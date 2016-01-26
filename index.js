8a0fb2326154ff5991b0bf5b90e13274a7f000b0
beeva-joseGutierrez <jose.gutierrez@beeva.com>
2016-01-26 12:03:34 +0100

    Dojo resolved 1st version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var long = 10;
+const long = 10;
 var Utils = require(__dirname+'/lib/Utils');
 var Arrays = Utils.prototype.Arrays(); // llamamos a función estática de Utils que nos devuelve la clase Arrays
 var ua = new Arrays(long); // llamamos a clase hija de Utils para arrays
